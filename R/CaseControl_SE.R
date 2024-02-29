5134fef20e65fe178a5e6f803b61fb2c38c24fc1
wolffha <150075196+wolffha@users.noreply.github.com>
2024-02-29 14:32:35 -0700

    Update CaseControl_SE.R
    Re-wrote the implementation of ReACt so it is significantly more computationally efficient (~20x)

--- R/CaseControl_SE.R
+++ R/CaseControl_SE.R
@@ -1,94 +1,3 @@
-# Helper function for ReACt method to derive case/control AF using SE
-# NOTE: this method exists as a C implementation at <https://github.com/Paschou-Lab/ReAct>
-GroupFreq <- function(se, nCase, nControl, OR, freq = 0.0) {
-  # using w, x, y, z to maintain consistency with the derivation from ReACt manuscript
-  x = 2*nCase
-  y = 2*nControl
-  w = se^2
-  z = OR
-  
-  
-  #inflate w(se) by 1.001, for at max 49 times (usually can be done within 5 iterations. 
-  #maximum inflation is 0.050195, ~5%), if tmp2 still < 0 then give up
-  for(i in (1:50)) {
-    w = w * (1.001^i)
-    tmp2 <- (((2*z*y*(1-z)) - (w*x*y*z))^2) - 4*(w*x*z + (1-z)^2)*(y*z*(x + y*z))
-    if (tmp2 >= 0) {
-      break
-    }
-  }
-  if (tmp2 < 0) { # if discriminate is <0 cannot solve and return 0s
-    resFrq <- data.frame(MAF_case = 0.0,
-                         MAF_control = 0.0,
-                         MAF_pop = 0.0)
-  } else { # actually solve the allele frequency
-    tmp1 <- w * x * y * z - (2 * y * z * (1 - z))
-    tmp2 = tmp2^0.5
-    tmp3 = 2*(w*x*z + (1-z)^2)
-    
-    d1 = (tmp1 - tmp2) / tmp3
-    c1 = y - d1
-    b1 = x * d1 / ((z * y) - (z * d1) + d1)
-    a1 = x - b1
-    frq1 = c1 / (c1 + d1)
-    
-    d2 = (tmp1 + tmp2)/ tmp3
-    c2 = y - d2
-    b2 = x * d2 / ((z * y) - (z * d2) + d2)
-    a2 = x - b2
-    frq2 = c2 / (c2 + d2)
-    
-    flag1 = 0 # indicator to use root d1
-    flag2 = 0 # indicator to use root d2
-
-    # need to select the root that allows a, b, c, d (allele counts of the 2x2 table) to all be positive
-    if (a1 > 0 & b1 > 0 & c1 > 0 & d1 > 0) { 
-      flag1 = 1
-      tmpRes1 <- data.frame(res11 = a1,
-                            res12 = b1,
-                            res21 = c1,
-                            res22 = d1)
-    }
-    if (a2 > 0 & b2 > 0 & c2 > 0 & d2 > 0) {
-      flag2 = 1
-      tmpRes2 <- data.frame(res11 = a2,
-                            res12 = b2,
-                            res21 = c2,
-                            res22 = d2)
-    }
-    
-    if (flag1 == 1 & flag2 == 0) { # use d1 if d1 is all positive and d2 is NOT
-      res = tmpRes1
-    } else if (flag1 == 0 & flag2 == 1) { # use d2 if all positive and d1 is NOT
-      res = tmpRes2
-    } else if (flag1 == 1 & flag2 == 1) { # if both are all positive then
-      if (abs(freq - frq1) < abs(freq - frq2)) { #use the larger value as it minimizes the fxn of interest
-        res = tmpRes1
-      } else {
-        res = tmpRes2
-      }
-    } else { # if it cannot solve due to negatives return all 0s
-      res =  data.frame(res11 = 0.0,
-                        res12 = 0.0,
-                        res21 = 0.0,
-                        res22 = 0.0)
-    }
-    
-    # solve for the minor allele frequency using the solved for allele counts
-    MAF_case = res$res11 / (res$res11 + res$res12)
-    MAF_control = res$res21 / (res$res21 + res$res22)
-    MAF_pop = (res$res11 + res$res21) / (res$res11 + res$res12 + res$res21 + res$res22)
-    
-    # if any evaluates to inf replace with 0
-    MAF_case <- ifelse(is.infinite(MAF_case), 0.0, MAF_case)
-    MAF_control <- ifelse(is.infinite(MAF_control), 0.0, MAF_control)
-    MAF_pop <- ifelse(is.infinite(MAF_pop), 0.0, MAF_pop)
-    
-    resFrq <- data.frame(MAF_case, MAF_control, MAF_pop)
-  }
-  return(resFrq)
-}
-
 #' @title CaseControl_SE
 #' @description This is a function to derive the case and control AFs from GWAS summary statistics when 
 #' the user has access to the whole sample AF, the sample sizes, and the OR (or beta).
@@ -99,14 +8,84 @@ GroupFreq <- function(se, nCase, nControl, OR, freq = 0.0) {
 #' @param nCase an integer of the number of Case individuals
 #' @param nControl an integer of the number of Control individuals
 #' @return a dataframe with 3 columns: MAF_case, MAF_control, MAF_pop for the estimated MAFs for each variant 
+#' @import genpwr
 #' @export 
 CaseControl_SE <- function(OR, SE, N_case, N_control) {
-  res <- data.frame(MAF_case = rep(0, length(OR)),
-                    MAF_control = rep(0, length(OR)),
-                    MAF_pop = rep(0, length(OR)))
+  # genpwr package is used to solve for the roots of the quadratic equation
+  require(genpwr)
+
+  # this function uses w, x, y, z as the derivation in the ReACt paper does - see their supplement
+  w = SE^2
+  x = 2*N_case
+  y = 2*N_control
+  z = OR
+
+  AC_control = rep(0, length(OR))
+  AC_case = rep(0, length(OR))
+
   for(i in 1:length(OR)) {
-    res[i,] <- GroupFreq(SE[i], nCase = N_case, nControl = N_control, OR[i])
-  }
-  
-  return(res)
+    # need to make sure the discriminate will be positibe or it won't solve
+  #inflate w(se) by 1.001, for at max 49 times (usually can be done within 5 iterations.
+  #maximum inflation is 0.050195, ~5%), if disc still < 0 then give up
+    for(j in 0:99) {
+      w[i] = w[i] * (1.001^j)
+      disc = (((2*z[i]*y*(1-z[i])) - (w[i]*x*y*z[i]))^2) - 4*(w[i]*x*z[i] + (1-z[i])^2)*(y*z[i]*(x + y*z[i]))
+      if (disc >= 0) {
+        break
+      } 
+    }
+    if (disc < 0) { # if discriminate is <0 cannot solve and return 0s
+      AC_control[i] = 0
+      AC_case[i] = 0
+    } else { # now actually solve the quadratic for allele counts (AC)
+        # solve for the a, b, and c of the quadratic equation
+        # this quadratic is solving for the allele count (AC) of the controls
+        # overall their derivation relies on AC (rather than AF) and then calculates AF
+        a = (w[i]*x*z[i]) + (1-z[i])^2
+        b = 2*y*z[i]*(1-z[i]) - w[i]*x*y*z[i]
+        c = y*z[i]*(x + y*z[i])
+        
+         #find roots of quadratic equation
+        AF_control_opts =  quad_roots(a, b, c)
+        # in order to select which root, we need to use each option (d1 and d2) to calculate a, b, c of the 2x2 table of allele counts
+        d1 = AF_control_opts[1]
+        c1 = y - d1
+        b1 = (x*d1)/(y*z[i] - z[i]*d1 + d1)
+        a1 = x - b1
+        
+        d2 = AF_control_opts[2]
+        c2 = y - d2
+        b2 = (x*d2)/(y*z[i] - z[i]*d2 + d2)
+        a2 = x - b2
+
+        vec1 = c(a1, b1, c1, d1) # vector of a,b,c,d using root 1
+        vec2 = c(a2, b2, c2, d2) # vector of a,b,c,d using root 2
+
+        # if both roots allow for all values to be positive, choose the larger
+        if(!any(vec1 < 0) & !(any(vec2 < 0))) {
+          if(d1 > d2) { # if d1 is the larger root, then the AC_control with be d1 and AC_case = a1
+            AC_control[i] = d1
+            AC_case[i] = a1
+          } else {
+            AC_control[i] = d2
+            AC_case[i] = a2
+          }
+        } else if(!any(vec1 < 0)) { # if d1 allows all values of 2x2 to be positive but NOT d2, use d1 and a1
+          AC_control[i] = d1
+          AC_case[i] = a1
+        } else { # if d2 allows all values to be positive but NOT d1, use d2 and a2
+          AC_control[i] = d2
+          AC_case[i] = a2
+        }
+      }
+    }
+
+  # calculate and return the MAF
+  MAF_case = AC_case/(2*N_case)
+  MAF_control = AC_control/(2*N_control)
+  MAF_pop = (MAF_case*N_case + MAF_control*N_control)/(N_case + N_control)
+
+  return(data.frame(MAF_case = MAF_case,
+                    MAF_control = MAF_control,
+                    MAF_pop = MAF_pop))
 }
